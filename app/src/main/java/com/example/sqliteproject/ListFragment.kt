4b2f23c0656eee49f3d20c5f4c0b9775469ea48a
dogaozyagci <75997342+dogaozyagci@users.noreply.github.com>
2023-02-22 16:55:10 +0300

    navigation added

--- app/src/main/java/com/example/sqliteproject/ListFragment.kt
+++ app/src/main/java/com/example/sqliteproject/ListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 class ListFragment : Fragment() {
 
@@ -12,6 +13,8 @@ class ListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+
+
 
         }
     }
