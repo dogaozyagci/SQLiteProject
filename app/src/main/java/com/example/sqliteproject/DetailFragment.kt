f19df9b4980c4b54be409c870930a1c10f413fb4
dogaozyagci <75997342+dogaozyagci@users.noreply.github.com>
2023-02-22 21:39:24 +0300

    permission added

--- app/src/main/java/com/example/sqliteproject/DetailFragment.kt
+++ app/src/main/java/com/example/sqliteproject/DetailFragment.kt
@@ -1,10 +1,14 @@
 package com.example.sqliteproject
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.sqliteproject.databinding.FragmentDetailBinding
 
 
@@ -44,7 +48,34 @@ class DetailFragment : Fragment() {
     }
 
     fun addPhoto(view: View){
-        println("görsel tıklandı")
+
+        activity?.let {
+            if(ContextCompat.checkSelfPermission(it.applicationContext,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION), 1)
+            } else {
+                val galeriIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent, 2)
+            }
+        }
+
+       }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1){
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val galeriIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntent, 2)
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        
     }
 
 
